c39f3752e7d4f8ca0978fcb055ce3ebeb5223ff8
PratikGK <59763924+Pratik-Hub@users.noreply.github.com>
2022-06-25 20:13:50 +0530

    Update ky-037-with-arduino.ino

--- ky-037-with-arduino.ino
+++ ky-037-with-arduino.ino
@@ -6,7 +6,7 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
+  int sensorValue = digitalRead(A1);
   // print out the value you read:
   Serial.println(sensorValue);
 } 
